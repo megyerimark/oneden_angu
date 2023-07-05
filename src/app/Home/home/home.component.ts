8c3fe21c00b671047929d91f67438830c98e146c
Kdorina <kovacsdorina2@gmail.com>
2023-07-05 14:41:40 +0200

    scroll button hozzáadása / formázása

--- src/app/Home/home/home.component.ts
+++ src/app/Home/home/home.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ReservationService } from 'src/app/Shared/reservation.service';
@@ -12,8 +12,26 @@ import Swal from 'sweetalert2';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+   this.scroll();
+  }
 
+  scroll(){
+    let upButton = document.getElementById('upButton') as HTMLElement;
+    window.onscroll = function scrollFunction(){
+      if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ){
+          upButton.style.display = "block";
+        }else{
+          upButton.style.display = "none";
+        }
+      }
+    }
+
+  topScroll(){
+      document.body.scrollTop = 0 ;
+      document.documentElement.scrollTop = 0;
+    }
 }
 
 
