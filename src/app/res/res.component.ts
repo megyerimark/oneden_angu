9bbb2761ee2e70018de393b525d613ca1fb99250
Kdorina <kovacsdorina2@gmail.com>
2023-07-05 13:10:05 +0200

    reservation ts megirasa

--- src/app/res/res.component.ts
+++ src/app/res/res.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-res',
@@ -6,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./res.component.scss']
 })
 export class ResComponent {
+  currentUser: any;
+
+
+  form!:FormGroup;
+  reservations: any;
+    constructor(
+      private router:Router,
+      private resService: NewType,
+      private http:HttpClient,
+      private formBuild:FormBuilder){}
+
+
+
+
+
+    ngOnInit(): void {
+      this.form = this.formBuild.group({
+        first_name:['', Validators.required],
+        email: ['', [Validators.required]],
+        phone_number:['', Validators.required],
+        res_date:['', Validators.required],
+        messages:['', Validators.required],
+
+      })
+    }
+
+
+
+  reservation(){
+    let resData = {
+      first_name : this.form.value.first_name,
+      email : this.form.value.email,
+      phone_number : this.form.value.phone_number,
+      res_date : this.form.value.res_date,
+      messages : this.form.value.messages,
+    }
+
+
+      this.resService.store(resData).subscribe({
+        next:(data:any) => {
+          /* Swal.fire(
+            'Sikeres Foglalás:',
+            data.data.first_name ,
+            'success'
+          );
+          this.form.reset(); */
+          console.log(data);
+        },
+        error:(err:any) => {
+          console.log('sikertelen foglalás')
+        }
+      });
+    }
 
 }
